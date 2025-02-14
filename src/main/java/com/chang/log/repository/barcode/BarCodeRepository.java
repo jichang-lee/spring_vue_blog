3140b0d794ad272d1f2ae7a3741e670706fd8c79
jichangLee <jccc23@naver.com>
2025-02-14 10:12:46 +0900

    fix : barcode logic delete

--- src/main/java/com/chang/log/repository/barcode/BarCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chang.log.repository.barcode;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarCodeRepository extends JpaRepository<BarCode,Long> {
-	Optional<BarCode> findByBarcode(String barcode);
-}
