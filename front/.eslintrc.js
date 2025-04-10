1597c3f68b513aa5fa9db4af748d5e6dd733eefd
jichangLee <116870832+jichang-lee@users.noreply.github.com>
2025-04-10 09:50:24 +0900

    Add ESLint configuration for Vue frontend

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,30 +1,53 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-        browser: true,
-    },
-    extends: [
-        'plugin:vue/vue3-recommended',
-        'eslint:recommended',
-        'prettier'
-    ],
-    parserOptions: {
-        parser: '@babel/eslint-parser',
-        ecmaVersion: 2020,
-    },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'vue/multi-word-component-names': 'off',
-        'vue/max-attributes-per-line': ['error', {
-            singleline: 3,
-            multiline: 1
-        }],
-        'vue/html-indent': ['error', 2],
-        'vue/script-indent': ['error', 2, { baseIndent: 0 }],
-        'indent': ['error', 2],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always']
-    }
+  root: true,
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'error',
+    'vue/no-unused-components': 'warn',
+    'vue/no-v-for-template-key-on-child': 'error',
+    'vue/no-unused-vars': 'warn',
+    'vue/no-mutating-props': 'error',
+    'vue/no-template-shadow': 'warn',
+    'vue/require-default-prop': 'warn',
+    'vue/require-prop-types': 'warn',
+    'vue/attribute-hyphenation': ['error', 'always'],
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
+    'vue/first-attribute-linebreak': ['error', {
+      'singleline': 'ignore',
+      'multiline': 'below'
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'always'
+    }],
+    'vue/html-indent': ['error', 2],
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 3,
+      'multiline': 1
+    }],
+    'vue/no-spaces-around-equal-signs-in-attribute': ['error'],
+    'vue/html-self-closing': ['warn', {
+      'html': {
+        'void': 'always',
+        'normal': 'never',
+        'component': 'always'
+      }
+    }],
+    'vue/prop-name-casing': ['error', 'camelCase'],
+    'vue/v-bind-style': ['error', 'shorthand'],
+    'vue/v-on-style': ['error', 'shorthand']
+  }
 };
